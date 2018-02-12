09c31062a2ab33aa19a66976f44beace95613da0
JasminMangei <jasmin.mangei@web.de>
2018-02-12 01:56:20 +0100

    Syntax angepasst

--- test.rb
+++ test.rb
@@ -1,20 +1,6 @@
 require 'test/unit'
 load 'pong.rb'
 
-
-
-	  #$x=Random.new.rand(0..100)
-	  #$y=Random.new.rand(0..100)
-	  #$richtung=100*Random.new.rand(0..1)
-		#$frame = Pong::Frame.new
-		#$testball = Pong::Ball.new $x, $y, $richtung, $frame	
-		#$test_x = $testball.x
-		#$test_y = $testball.y
-		#$test_yachsenabschnitt = $testball.instance_variable_get(:@yachsenabschnitt)
-		#$test_steigung = $testball.instance_variable_get(:@steigung)
-		#$paddle=Pong::Paddle.new
-	
-		
 class Zutestendes
 	
 	
@@ -50,23 +36,7 @@ class Zutestendes
 		@scorerechts = @frame.instance_variable_get(:@score_right)
 		@scorelinks = @frame.instance_variable_get(:@score_left)
 	end
-=begin
-	def get_x
-		return @test_x
-	end
-	
-	def get_y
-		return @test_y
-	end
-	
-	def get_steigung
-		return @test_steigung
-	end
-	
-	def get_yachsenabschnitt
-		return @yachsenabschnitt
-	end
-=end	
+
 end	
 		
 
@@ -92,14 +62,13 @@ class TestPong < Test::Unit::TestCase
 
 	def test_x_wert_des_balles
 		#Annahme: def x  und (y-yachsenabschnitt)/steigung sind dasselbe
-		#puts "beispiel #{@beispiel.instance_variable_get(:@test_x)}"
 		if @steigungwert == 0
 			#assert_equal( )
 		else
 	 	  assert_equal(@xwert, (@ywert-@yachsenabschnittwert)/@steigungwert)
 		end
 	end
-=begin		
+
 	def test_y_wert_des_balles
 		#Annahme def y und steigung*x+yachsenabschnitt sind dasselbe
 		assert_equal(@ywert, @steigungwert * @xwert + @yachsenabschnittwert)
@@ -107,15 +76,15 @@ class TestPong < Test::Unit::TestCase
 			
 	def test_Ball_trifft_auf_Ecke_des_Spielfeldes
 		# Abfrage nach dem Fall, dass Ball in Ecke fliegt
-		if (@beispiel.test_x== 0 and @beispiel.test_y==0) or (@xwert==100 or @beispiel.test_y==0) or (beispiel.test_x==0 and @beispiel.test_y==100) or (@beispiel.test_x==100 and @beispiel.test_y==100) then
-			if @beispiel.test_x==0 then
-				assert_equal(@beispiel.test_x, 0)
-			elsif @beispiel.test_x==100
-				assert_equal(@beispiel.test_x, 100)
-			elsif @beispiel.test_y==0
-				assert_equal(@beispiel.test_y, 0)
+		if (@xwert== 0 and @ywert=0) or (@xwert==100 or @ywert==0) or (@xwert==0 and @ywert==100) or (@xwert==100 and @ywert==100) then
+			if @bxwert==0 then
+				assert_equal(@xwert, 0)
+			elsif @xwert==100
+				assert_equal(@xwert, 100)
+			elsif @ywert==0
+				assert_equal(@ywert, 0)
 			else 
-				assert_equal(@beispiel.test_y, 100)
+				assert_equal(@ywert, 100)
 			end
 		end
 		
@@ -131,13 +100,13 @@ class TestPong < Test::Unit::TestCase
 	
 
 	def test_Ball_trifft_Rand_des_Paddels_rechts
-		aktuelle_steigung=@beispiel.instance_variable_get(:@test_steigung)
-		if @beispiel.paddleright.collision(@beispiel.instance_variable_get(:@test_y))!=0 and @beispiel.paddleright.collision(@beispiel.instance_variable_get(test_y))!=nil then
-			assert_greater_and_equal_than(@beispiel.instance_variable_get(:@test_steigung), @aktuelle_steigung)
+		@aktuelle_steigung=@steigungwert
+		if @collision!=0 and @collision!=nil then
+			assert_greater_and_equal_than(@steigungwert, @aktuelle_steigung)
 		end
 	end
 	
-=end	
+
 	
 	def test_Ball_trifft_nicht_Paddel_rechts
 		@score=@scorer
